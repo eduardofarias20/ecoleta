7082bfaf5152b835fdf86e00c372a5a506366c0a
eduardofarias20 <edu.farias8@hotmail.com>
2020-06-04 15:52:29 -0300

    Projeto NWL Ecoleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -11,5 +11,5 @@ routes.get('/items', itemsController.index);
 routes.post('/points', pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
-
+ 
 export default routes;
